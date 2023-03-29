ea9b747c5f57fdaefb833df79b37e16b5dbc471a
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-29 19:34:26 -0400

    adds the other feature cards for manage portfolio and dashboard

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,29 @@ function Home() {
   return (
     <main className="home-page">
       <Hero />
-      <FeatureCard icon={marketsIcon} featureTitle={"Markets"} featureDescription={"View real-time price data of the cryptocurrency market"}/>
+      <div className="home-page__feature-cards-container">
+        <FeatureCard
+          icon={marketsIcon}
+          featureTitle={"Markets"}
+          featureDescription={
+            "View real-time price data of the cryptocurrency market"
+          }
+        />
+        <FeatureCard
+          icon={portfolioIcon}
+          featureTitle={"Manage Portfolio"}
+          featureDescription={
+            "Update your portfolio holdings when you buy or sell investments. This way, Portfolio Sphere can kee your portfolio up to date"
+          }
+        />
+        <FeatureCard
+          icon={dashboardIcon}
+          featureTitle={"Dashboard"}
+          featureDescription={
+            "Valuable insights into your portfolio. View the allocation of your portfolio among your top holdings and the total value of your investments"
+          }
+        />
+      </div>
     </main>
   );
 }
