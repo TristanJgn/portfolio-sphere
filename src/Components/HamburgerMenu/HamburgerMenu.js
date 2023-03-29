dc2a15c0499474ee2ef6f864c32dca691ab5c421
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-29 13:30:02 -0400

    adds function to toggle active class on click of hamburger menu as well as styling

--- src/Components/HamburgerMenu/HamburgerMenu.js
+++ src/Components/HamburgerMenu/HamburgerMenu.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./HamburgerMenu.scss";
 
 function HamburgerMenu() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    isActive ? setIsActive(false) : setIsActive(true);
+  };
 
   return (
-    <div className="menu-container">
-      <div className="hamburger-menu">
-        <span className="hamburger-menu__line"></span>
-        <span className="hamburger-menu__line"></span>
-        <span className="hamburger-menu__line"></span>
+    <div className={`menu-container`} onClick={toggleActive}>
+      <div
+        className={`hamburger-menu ${isActive ? "hamburger-menu--active" : ""}`}
+      >
+        <span
+          className={`hamburger-menu__line ${
+            isActive ? "hamburger-menu__line--active" : ""
+          }`}
+        ></span>
+        <span
+          className={`hamburger-menu__line ${
+            isActive ? "hamburger-menu__line--active" : ""
+          }`}
+        ></span>
+        <span
+          className={`hamburger-menu__line ${
+            isActive ? "hamburger-menu__line--active" : ""
+          }`}
+        ></span>
       </div>
       <nav>
-        <ul className="menu">
+        <ul className={`menu ${isActive ? "menu--active" : ""}`}>
           <li className="menu__item">
             <NavLink to="/markets" className="menu__link">
               Markets
