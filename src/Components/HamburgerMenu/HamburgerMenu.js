7a894a1678be85616783915c7bfa017eb67f2f52
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-29 12:59:33 -0400

    adds structure of menu with each item being linked to its respective page using NavLink from react-router-dom

--- src/Components/HamburgerMenu/HamburgerMenu.js
+++ src/Components/HamburgerMenu/HamburgerMenu.js
@@ -1,7 +1,41 @@
+import { NavLink } from "react-router-dom";
 import "./HamburgerMenu.scss";
 
 function HamburgerMenu() {
-  return <h1>This is the Hamburger Menu</h1>;
+
+  return (
+    <div className="menu-container">
+      <div className="hamburger-menu">
+        <span className="hamburger-menu__line"></span>
+        <span className="hamburger-menu__line"></span>
+        <span className="hamburger-menu__line"></span>
+      </div>
+      <nav>
+        <ul className="menu">
+          <li className="menu__item">
+            <NavLink to="/markets" className="menu__link">
+              Markets
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink to="/portfolio" className="menu__link">
+              Manage Portfolio
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink to="/dashboard" className="menu__link">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink to="/login" className="menu__link">
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default HamburgerMenu;
