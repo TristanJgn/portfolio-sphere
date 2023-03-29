c2427be9c7ed48c49f83d1e8376dab50b949bf8b
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-29 14:40:17 -0400

    adds icons as well as option to return home in hamburger menu

--- src/Components/HamburgerMenu/HamburgerMenu.js
+++ src/Components/HamburgerMenu/HamburgerMenu.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import homeIcon from "../../assets/icons/home.svg";
+import marketsIcon from "../../assets/icons/markets.svg"
+import portfolioIcon from "../../assets/icons/portfolio.svg"
+import dashboardIcon from "../../assets/icons/dashboard.svg";
+import loginIcon from "../../assets/icons/login.svg";
 import "./HamburgerMenu.scss";
 
 function HamburgerMenu() {
@@ -33,23 +38,53 @@ function HamburgerMenu() {
       <nav>
         <ul className={`menu ${isActive ? "menu--active" : ""}`}>
           <li className="menu__item">
+            <NavLink to="/" className="menu__link">
+              <img
+                className="menu__icon menu__icon--bigger"
+                src={homeIcon}
+                alt="markets icon"
+              />
+              <p className="menu__text">Home</p>
+            </NavLink>
+          </li>
+          <li className="menu__item">
             <NavLink to="/markets" className="menu__link">
-              Markets
+              <img
+                className="menu__icon"
+                src={marketsIcon}
+                alt="markets icon"
+              />
+              <p className="menu__text">Markets</p>
             </NavLink>
           </li>
           <li className="menu__item">
             <NavLink to="/portfolio" className="menu__link">
-              Manage Portfolio
+              <img
+                className="menu__icon menu__icon--bigger"
+                src={portfolioIcon}
+                alt="portfolio icon"
+              />
+              <p className="menu__text">Manage Portfolio</p>
             </NavLink>
           </li>
           <li className="menu__item">
             <NavLink to="/dashboard" className="menu__link">
-              Dashboard
+              <img
+                className="menu__icon"
+                src={dashboardIcon}
+                alt="dashboard icon"
+              />
+              <p className="menu__text">Dashboard</p>
             </NavLink>
           </li>
           <li className="menu__item">
             <NavLink to="/login" className="menu__link">
-              Login
+              <img
+                className="menu__icon menu__icon--bigger"
+                src={loginIcon}
+                alt="login icon"
+              />
+              <p className="menu__text">Login</p>
             </NavLink>
           </li>
         </ul>
