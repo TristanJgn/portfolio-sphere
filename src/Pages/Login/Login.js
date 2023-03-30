09015bbb746ee207a08187e44a26acfba2bc57aa
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-30 03:02:00 -0400

    adds form styling along with states to track errors and conditional classes based on error states

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,6 +5,8 @@ import "./Login.scss";
 function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
 
   const handleChangeEmail = (event) => {
       setEmail(event.target.value);
@@ -14,15 +16,21 @@ function Login() {
     setPassword(event.target.value);
   };
 
+
+
   return (
     <main className="login-page">
       <section className="login-form-container">
-        <h2 className="login-form-container__title">Sign In</h2>
+        <h2 className="login-form-container__title">SIGN IN</h2>
         <form className="login-form">
           <div className="login-form__input-group">
-            <label className="login-form__label" htmlFor="email"></label>
+            <label className="login-form__label" htmlFor="email">
+              Email
+            </label>
             <input
-              className="login-form__input"
+              className={`login-form__input ${
+                validEmail === "error" ? "login-form__input--error" : ""
+              }`}
               type="text"
               id="email"
               name="email"
@@ -30,21 +38,33 @@ function Login() {
               value={email}
               onChange={handleChangeEmail}
             ></input>
-            <div className={`login-form-error`}>
+            <div
+              className={`login-form-error ${
+                validEmail === "error" ? "login-form-error--show" : ""
+              }`}
+            >
               <img
                 className="login-form-error__icon"
                 src={errorIcon}
                 alt="error icon"
               />
               <h3 className="login-form-error__message">
-                This field is required
+                {`${
+                  email && validEmail === "error"
+                    ? "Please enter a valid email"
+                    : "This field is required"
+                }`}
               </h3>
             </div>
           </div>
           <div className="login-form__input-group">
-            <label className="login-form__label" htmlFor="password"></label>
+            <label className="login-form__label" htmlFor="password">
+              Password
+            </label>
             <input
-              className="login-form__input"
+              className={`login-form__input ${
+                validPassword === "error" ? "login-form__input--error" : ""
+              }`}
               type="password"
               id="password"
               name="password"
@@ -52,7 +72,11 @@ function Login() {
               value={password}
               onChange={handleChangePassword}
             ></input>
-            <div className={`login-form-error`}>
+            <div
+              className={`login-form-error ${
+                validPassword === "error" ? "login-form-error--show" : ""
+              }`}
+            >
               <img
                 className="login-form-error__icon"
                 src={errorIcon}
@@ -63,6 +87,7 @@ function Login() {
               </h3>
             </div>
           </div>
+          <button className="login-form__button">Login</button>
         </form>
       </section>
     </main>
