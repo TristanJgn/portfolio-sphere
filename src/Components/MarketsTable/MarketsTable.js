5ebd06865f49649aea953847dfae8737d369afea
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-03 15:38:01 -0400

    adds reworked styling to manage overflow and sticky columns without the use of an unnecessary div

--- src/Components/MarketsTable/MarketsTable.js
+++ src/Components/MarketsTable/MarketsTable.js
@@ -24,86 +24,83 @@ function MarketsTable() {
   }
 
   return (
-    <>
-      <div className="hidden-overflow"></div>
-      <div className="markets-table-container">
+    <div className="markets-table-container">
         <table className="markets-table">
-          <thead className="markets-table__header-container">
+            <thead className="markets-table__header-container">
             <tr className="markets-table__header-row">
-              <th className="markets-table__header-column markets-table__header-column--sticky--first">
+                <th className="markets-table__header-column markets-table__header-column--sticky--first">
                 Rank
-              </th>
-              <th className="markets-table__header-column markets-table__header-column--sticky--second">
+                </th>
+                <th className="markets-table__header-column markets-table__header-column--sticky--second">
                 Coin
-              </th>
-              <th className="markets-table__header-column">Price</th>
-              <th className="markets-table__header-column">1h</th>
-              <th className="markets-table__header-column">24h</th>
-              <th className="markets-table__header-column">7d</th>
-              <th className="markets-table__header-column">Market Cap</th>
-              <th className="markets-table__header-column">24h Volume</th>
+                </th>
+                <th className="markets-table__header-column">Price</th>
+                <th className="markets-table__header-column">1h</th>
+                <th className="markets-table__header-column">24h</th>
+                <th className="markets-table__header-column">7d</th>
+                <th className="markets-table__header-column">Market Cap</th>
+                <th className="markets-table__header-column">24h Volume</th>
             </tr>
-          </thead>
-          <tbody className="markets-table__body-container">
+            </thead>
+            <tbody className="markets-table__body-container">
             {coinsList.map((coin, index) => {
                 return (
-                  <tr className="markets-table__body-row">
+                    <tr className="markets-table__body-row">
                     <td className="markets-table__body-column markets-table__body-column--sticky--first">
-                      {index + 1}
+                        {index + 1}
                     </td>
                     <td className="markets-table__body-column markets-table__body-column--sticky--second">
-                      <div className="markets-table__body-column-coin">
+                        <div className="markets-table__body-column-coin">
                         <p className="markets-table__body-column-coin__name">
-                          {coin.name}
+                            {coin.name}
                         </p>
                         <p className="markets-table__body-column-coin__symbol">
-                          {coin.symbol}
+                            {coin.symbol}
                         </p>
-                      </div>
+                        </div>
                     </td>
                     <td className="markets-table__body-column">
-                      {priceFormat(coin.price)}
+                        {priceFormat(coin.price)}
                     </td>
                     <td
-                      className={`markets-table__body-column ${
+                        className={`markets-table__body-column ${
                         coin.percent_change_1h > 0
-                          ? "markets-table__body-column--positive"
-                          : "markets-table__body-column--negative"
-                      }`}
+                            ? "markets-table__body-column--positive"
+                            : "markets-table__body-column--negative"
+                        }`}
                     >
-                      {percentageFormat(coin.percent_change_1h)}
+                        {percentageFormat(coin.percent_change_1h)}
                     </td>
                     <td
-                      className={`markets-table__body-column ${
+                        className={`markets-table__body-column ${
                         coin.percent_change_24h > 0
-                          ? "markets-table__body-column--positive"
-                          : "markets-table__body-column--negative"
-                      }`}
+                            ? "markets-table__body-column--positive"
+                            : "markets-table__body-column--negative"
+                        }`}
                     >
-                      {percentageFormat(coin.percent_change_24h)}
+                        {percentageFormat(coin.percent_change_24h)}
                     </td>
                     <td
-                      className={`markets-table__body-column ${
+                        className={`markets-table__body-column ${
                         coin.percent_change_7d > 0
-                          ? "markets-table__body-column--positive"
-                          : "markets-table__body-column--negative"
-                      }`}
+                            ? "markets-table__body-column--positive"
+                            : "markets-table__body-column--negative"
+                        }`}
                     >
-                      {percentageFormat(coin.percent_change_7d)}
+                        {percentageFormat(coin.percent_change_7d)}
                     </td>
                     <td className="markets-table__body-column">
-                      {roundedDollarFormat(coin.market_cap)}
+                        {roundedDollarFormat(coin.market_cap)}
                     </td>
                     <td className="markets-table__body-column">
-                      {roundedDollarFormat(coin.volume_24h)}
+                        {roundedDollarFormat(coin.volume_24h)}
                     </td>
-                  </tr>
+                    </tr>
                 );
             })}
-          </tbody>
+            </tbody>
         </table>
-      </div>
-    </>
+    </div>
   );
 }
 
