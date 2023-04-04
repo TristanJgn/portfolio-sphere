e01d534d0932ac45c081cf65f707a8bd9fecec9a
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-04 18:51:57 -0400

    adds delete modal with styling

---
+++ src/Components/DeleteModal/DeleteModal.js
@@ -0,0 +1,43 @@
+import closeIcon from "../../assets/icons/close-white.svg";
+import "./DeleteModal.scss";
+
+function DeleteModal({ coin, show, onClose }) {
+  if (show === false) {
+    return null;
+  }
+
+  return (
+    <div className="delete-modal" onClick={() => onClose()}>
+      <div
+        className="delete-modal__content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button className="delete-modal__close" onClick={() => onClose()}>
+          <img src={closeIcon} alt="close icon" />
+        </button>
+        <div className="delete-modal__top-container">
+          <div className="delete-modal__header">
+            <h1 className="delete-modal__header-title">
+              Delete <strong>{coin.coin_name}</strong>?
+            </h1>
+          </div>
+          <div className="delete-modal__body">
+            <p className="delete-modal__body-p">
+              Please confirm that you'd like to delete{" "}
+              <strong>{coin.coin_name}</strong> from your portfolio. You won't
+              be able to undo this action.
+            </p>
+          </div>
+        </div>
+        <div className="delete-modal__footer">
+          <button className="delete-modal__cancel" onClick={() => onClose()}>
+            Cancel
+          </button>
+          <button className="delete-modal__delete">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteModal;
