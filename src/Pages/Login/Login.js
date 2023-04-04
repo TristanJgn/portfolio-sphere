9622ee274f6d04036aef4da8813f5d4872cda527
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-03 23:50:56 -0400

    adds axios post request on login with error message if the user is trying with invalid credentials, adds JWT token to session storage and navigates the user back home on a successful login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Login.scss";
 
 function Login() {
@@ -7,6 +8,9 @@ function Login() {
   const [password, setPassword] = useState("")
   const [validEmail, setValidEmail] = useState(false);
   const [validPassword, setValidPassword] = useState(false);
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleChangeEmail = (event) => {
       setEmail(event.target.value);
@@ -48,7 +52,28 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    validateForm();
+    setInvalidCredentials(false); // Clear the invalid credentials error message on every form submission attempt, if the information is invalid again, the catch statement will handle it
+    if (validateForm()) {
+      axios
+        .post("http://localhost:8080/users/login", {
+          email,
+          password,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            sessionStorage.setItem("authToken", response.data.token); // Set session to have JWT token so user can start using the site
+            setTimeout(() => {
+              navigate("/"); // Take user back home after successful login
+            }, 2000);
+          }
+        })
+        .catch((error) => {
+          if (error.response.data.error === "Invalid login credentials") {
+            setInvalidCredentials(true);
+          }
+          return <h2>{error.message}</h2>;
+        });
+    }
   };
 
   return (
@@ -89,6 +114,15 @@ function Login() {
                 }`}
               </h3>
             </div>
+            <div
+              className={`register-form-error ${
+                invalidCredentials ? "register-form-error--show" : ""
+              }`}
+            >
+              <h3 className="register-form-error__message">
+                Invalid login credentials
+              </h3>
+            </div>
           </div>
           <div className="login-form__input-group">
             <label className="login-form__label" htmlFor="password">
@@ -111,10 +145,16 @@ function Login() {
               }`}
             >
               <h3 className="login-form-error__message">
-                {`${!password
-                    ? "This field is required"
-                    : ""
-                }`}
+                {`${!password ? "This field is required" : ""}`}
+              </h3>
+            </div>
+            <div
+              className={`register-form-error ${
+                invalidCredentials ? "register-form-error--show" : ""
+              }`}
+            >
+              <h3 className="register-form-error__message">
+                Invalid login credentials
               </h3>
             </div>
           </div>
