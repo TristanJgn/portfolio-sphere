a7489de57e255d3b13e6b5405bc64f6f99a4f5b8
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-04 12:12:15 -0400

    adds page view when a user is not logged in letting them know they must login and giving a CTA button to take them to the login page

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import MainButton from "../../Components/Buttons/MainButton/MainButton"
 import "./Portfolio.scss";
 
 function Portfolio() {
-  return (
-    <main className="portfolio-page">
-      <h1>This is the Portfolio Page</h1>
-    </main>
-  );
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login"); // Take the user to the login page
+  };
+
+  if (!sessionStorage.authToken) {
+    return (
+      <main className="portfolio-page">
+        <h1 className="portfolio-page__title">
+          You must be logged in to view your portfolio.
+        </h1>
+        <MainButton
+          buttonText={"Login"}
+          onClickAction={handleLogin}
+        />
+      </main>
+    );
+  }
+
 }
 
 export default Portfolio;
