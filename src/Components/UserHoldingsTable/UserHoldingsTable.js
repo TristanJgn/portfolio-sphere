e830ad317275a62624f5595efdfcd8efca0d4840
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-04 22:38:31 -0400

    moves style files into a partials folder and updates all links

--- src/Components/UserHoldingsTable/UserHoldingsTable.js
+++ src/Components/UserHoldingsTable/UserHoldingsTable.js
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import UserHoldingsTableRow from "../UserHoldingsTableRow/UserHoldingsTableRow";
+import searchIcon from "../../assets/icons/search.svg"
 import "./UserHoldingsTable.scss";
 
 function UserHoldingsTable({ userHoldings, coinsList }) {
+  const [searchedCoin, setSearchedCoin] = useState("");
+
+  const handleChange = (event) => {
+    setSearchedCoin(event.target.value);
+  }
+
+
   return (
-    <div className="user-holdings-table-container">
-      <table className="user-holdings-table">
-        <thead className="user-holdings-table__header-container">
-          <tr className="user-holdings-table__header-row">
-            <th className="user-holdings-table__header-column user-holdings-table__header-column--left">
-              Coin
-            </th>
-            <th className="user-holdings-table__header-column">Amount</th>
-            <th className="user-holdings-table__header-column">Actions</th>
-          </tr>
-        </thead>
-        <tbody className="user-holdings-table__body-container">
-          {userHoldings.map((coin) => (
-            <UserHoldingsTableRow key={coin.coin_id} coin={coin} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <section className="user-holdings-section">
+      <div className="user-holdings-section__search">
+        <img
+          className="user-holdings-section__icon"
+          src={searchIcon}
+          alt="search icon"
+        />
+        <input
+          className="user-holdings-section__input"
+          type="text"
+          id="coinSearch"
+          name="coinSearch"
+          placeholder="Search Portfolio..."
+          onChange={handleChange}
+          value={searchedCoin}
+        ></input>
+      </div>
+      <div className="user-holdings-table-container">
+        <table className="user-holdings-table">
+          <thead className="user-holdings-table__header-container">
+            <tr className="user-holdings-table__header-row">
+              <th className="user-holdings-table__header-column user-holdings-table__header-column--left">
+                Coin
+              </th>
+              <th className="user-holdings-table__header-column">Amount</th>
+              <th className="user-holdings-table__header-column">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="user-holdings-table__body-container">
+            {userHoldings.map((coin) => (
+              <UserHoldingsTableRow key={coin.coin_id} coin={coin} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
 
