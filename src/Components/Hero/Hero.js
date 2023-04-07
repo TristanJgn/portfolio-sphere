45b7cf4697399ba452bfb8b7c2d4ec72d344e4fe
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-07 18:36:25 -0400

    adds adjustments to hero section with a placeholder image

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import heroImage from "../../assets/images/Hero.png"
 import MainButton from "../MainButton/MainButton";
+import heroImage from "../../assets/images/Hero.png";
+import placeholderImage from "../../assets/images/Placeholder.jpg"
 import "./Hero.scss";
 
 function Hero() {
@@ -11,27 +12,34 @@ function Hero() {
 
   return (
     <section className="hero">
-      <div className="hero__text-container">
-        <h1 className="hero__main-text">
-          Keep <span className="hero__main-text--special">track of all</span>{" "}
-          your cryptocurrency holdings in{" "}
-          <span className="hero__main-text--special">one place</span>
-        </h1>
-        <h2 className="hero__sub-text--tablet">
-          Gathering insights into all of your investments has never been easier
+      <div className="hero__left-container">
+        <div className="hero__text-container">
+          <h1 className="hero__main-text">
+            Keep <span className="hero__main-text--special">track of all</span>{" "}
+            your cryptocurrency holdings in{" "}
+            <span className="hero__main-text--special">one place</span>
+          </h1>
+          <h2 className="hero__sub-text--tablet">
+            Gathering insights into all of your investments has never been
+            easier
+          </h2>
+        </div>
+        <div className="hero__sub-image-container">
+          <img className="hero__sub-image" src={heroImage} alt="hero circle" />
+        </div>
+        <h2 className="hero__sub-text">
+          <span className="hero__sub-text--special">Gathering insights</span>{" "}
+          into all of your investments has{" "}
+          <span className="hero__sub-text--special">never been easier</span>
         </h2>
+        <MainButton
+          buttonText={"Get Started"}
+          onClickAction={handleGetStarted}
+        />
       </div>
-      <div className="hero__image-container">
-        <img className="hero__image" src={heroImage} alt="hero" />
+      <div className="hero__main-image-container">
+        <img className="hero__main-image" src={placeholderImage} alt="hero main" />
       </div>
-      <h2 className="hero__sub-text">
-        <span className="hero__sub-text--special">Gathering insights</span> into
-        all of your investments has{" "}
-        <span className="hero__sub-text--special">
-          never been easier
-        </span>
-      </h2>
-      <MainButton buttonText={"Get Started"} onClickAction={handleGetStarted}/>
     </section>
   );
 }
