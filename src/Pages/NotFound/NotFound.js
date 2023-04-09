07beb9fe500d466af2fe720ff6b31ab4df2daceb
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-09 17:18:36 -0400

    adds not found page with styling

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import MainButton from "../../Components/MainButton/MainButton";
 import "./NotFound.scss";
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleReturnHome = () => {
+    navigate("/"); // Takes the user back to the home page
+  }
+
   return (
     <main className="not-found-page">
-      <h1>This is the Not Found Page</h1>
+      <div className="not-found-page__container">
+        <h1 className="not-found-page__title">
+          404
+        </h1>
+        <h2 className="not-found-page__sub-title">
+          Page not found
+        </h2>
+        <h3 className="not-found-page__text">
+          We're sorry, the page you are looking for does not exist.
+        </h3>
+        <MainButton
+          buttonText={"Back to Home"}
+          onClickAction={handleReturnHome}
+        />
+      </div>
     </main>
   );
 }
